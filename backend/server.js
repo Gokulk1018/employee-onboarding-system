5c382c50e00df6a9d48609f04d66f9e10720f223
Gokulk1018 <gokulk.1018@gmail.com>
2026-02-10 09:41:53 +0530

    backend start

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to database
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Backend Server Running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
